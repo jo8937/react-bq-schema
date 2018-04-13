8a5722d5879e82380f7ce09d583be8f0f7b8699a
JiHoonCho <jihooncho@george-mac-15.local>
2018-04-13 22:41:40 +0900

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p className="App-intro">
-          <Button color="danger">Danger!</Button>
+          <Button color="danger">Danger!..</Button>
         </p>
       </div>
     );
