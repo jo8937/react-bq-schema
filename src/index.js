3f3d2bcd8eeee605aced2031a01eb696545f3142
george <jo8937@gmail.com>
2018-04-16 16:01:30 +0900

    customize bootstrap4 color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-//import "bootstrap/scss/bootstrap.scss"
 import './styles/scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
